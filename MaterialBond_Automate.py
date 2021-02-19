1056a7275a2c0531b36547c4ee1207f0099f5700
Tan Chia Chun <54241577+TanChiaChun@users.noreply.github.com>
2021-02-20 01:15:00 +0800

    Add comment, log, exception

--- MaterialBond_Automate.py
+++ MaterialBond_Automate.py
@@ -6,6 +6,7 @@ from glob import glob
 from shutil import move
 from xml.etree import ElementTree
 from pywinauto import application
+from pywinauto.findwindows import ElementNotFoundError
 
 # Import from modules
 from MyFunctions import initialise_app, finalise_app, handle_exception
@@ -45,24 +46,52 @@ app_path = args.app
 ##################################################
 # Main
 ##################################################
+# Validate arguments
+if not os.path.exists(app_path):
+    handle_exception("Application not found!")
+if not os.path.exists(xml_folder):
+    handle_exception("XML folder not found!")
+
+# Create archive folder
 os.makedirs(xml_folder_archive, exist_ok=True)
 
+# Get list of XMLs and sort descending by date
 xmls = sorted(glob(xml_folder + "/*.xml"), key=os.path.getmtime)
-
 if len(xmls) == 0:
     finalise_app("XML folder empty! No part to process")
 
-tree = ElementTree.parse(xmls[0])
-root = tree.getroot()
-for child1 in root:
-    for child2 in child1:
-        data_dict[child2.tag] = child2.text
+# Parse XML and add data to dict
+selected_xml_path = xmls[0]
+selected_xml = os.path.basename(selected_xml_path)
+logger.info("Read " + selected_xml)
+try:
+    tree = ElementTree.parse(selected_xml_path)
+    root = tree.getroot()
+    for child1 in root:
+        for child2 in child1:
+            data_dict[child2.tag] = child2.text
+except ElementTree.ParseError:
+    handle_exception("XML file not recognised!")
+except Exception:
+    handle_exception("XML file error!")
 
+# Start external app and input data
+logger.info("Starting " + os.path.basename(app_path))
 app = application.Application(backend="uia").start(app_path)
-app["Material Bond"]["Serial NumberEdit"].type_keys(data_dict["SerialNumber"])
-app["Material Bond"]["Material AEdit"].type_keys(data_dict["MaterialA"])
-app["Material Bond"]["Material BEdit"].type_keys(data_dict["MaterialB"])
+try:
+    logger.debug("Inputting Serial Number: " + data_dict["SerialNumber"])
+    app["Material Bond"]["Serial NumberEdit"].type_keys(data_dict["SerialNumber"])
+    logger.debug("Inputting Material A: " + data_dict["MaterialA"])
+    app["Material Bond"]["Material AEdit"].type_keys(data_dict["MaterialA"])
+    logger.debug("Inputting Material B: " + data_dict["MaterialB"])
+    app["Material Bond"]["Material BEdit"].type_keys(data_dict["MaterialB"])
+except ElementNotFoundError:
+    handle_exception("Error communicating with Material Bond application!")
+except Exception:
+    handle_exception("Material Bond application error")
 
-move(xmls[0], xml_folder_archive + '/' + os.path.basename(xmls[0]))
+# Archive XML
+move(selected_xml_path, xml_folder_archive + '/' + selected_xml)
+logger.info("Archived "+ selected_xml)
 
 finalise_app()
